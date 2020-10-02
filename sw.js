ac924c82a088f45399b245e0b32a8e5cb77d08c5
root <root@IP-MTY-BECERRIL.mty.ipointsystems.com>
2020-10-02 07:49:16 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7a4afb364c9692dc2a91.js"
+    "url": "webpack-runtime-772d06548c73b889c60a.js"
   },
   {
     "url": "framework-5a1b297d9adac74fe592.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "744d6c78002bd0e2e7350d887d44f2ac"
+    "revision": "989573f6aac717ae6c231ecb714a433a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
